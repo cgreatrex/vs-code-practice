fcd5b52314754398a593e3154ea20d22830bf248
cgreatrex <cgreatre@uwo.ca>
2026-02-15 17:37:01 -0500

    folder organization agent

---
+++ tests/test_example.py
@@ -0,0 +1,12 @@
+"""Example test file"""
+
+
+def test_sample():
+    """Sample test case"""
+    assert True
+
+
+def test_another():
+    """Another sample test"""
+    result = 1 + 1
+    assert result == 2
